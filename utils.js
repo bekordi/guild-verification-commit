529364f02c7c4e0e7b0f285852beb3714ae8d618
bekordi <150627764+bekordi@users.noreply.github.com>
2026-03-05 21:59:26 +0300

    Patch API endpoint error handling

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:59:22.526Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T18:59:25.549Z
+Commit: Patch API endpoint error handling
