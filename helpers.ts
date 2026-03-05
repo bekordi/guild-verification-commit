e242ec865f99a0df7983c8791bc0d3ac4c78fe3b
bekordi <150627764+bekordi@users.noreply.github.com>
2026-03-05 21:59:16 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:59:09.271Z
-Commit: Resolve timezone issues in date handling
+Timestamp: 2026-03-05T18:59:15.553Z
+Commit: Fix memory leak in event handlers
