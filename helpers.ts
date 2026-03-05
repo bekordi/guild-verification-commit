bf08495971b30d7178e6918702d6cf7bd8125134
bekordi <150627764+bekordi@users.noreply.github.com>
2026-03-05 21:58:46 +0300

    Fix database connection timeout

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:58:33.882Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T18:58:45.377Z
+Commit: Fix database connection timeout
