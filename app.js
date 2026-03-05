2602e8f45aee879a6a2048093a9a772679f1d003
bekordi <150627764+bekordi@users.noreply.github.com>
2026-03-05 21:58:50 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:58:36.831Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-03-05T18:58:49.844Z
+Commit: Fix broken links in documentation
