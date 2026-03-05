c529811a507c6499dcd3b662116411da71095581
bekordi <150627764+bekordi@users.noreply.github.com>
2026-03-05 21:59:27 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:59:25.549Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-05T18:59:26.944Z
+Commit: Resolve null pointer exception in user service
