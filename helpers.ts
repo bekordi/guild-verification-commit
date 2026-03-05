c6bc7cf07ea4f27623b64f9df6d244e750cbfc9a
bekordi <150627764+bekordi@users.noreply.github.com>
2026-03-05 21:58:35 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:58:17.506Z
-Commit: Add export functionality for reports
+Timestamp: 2026-03-05T18:58:33.882Z
+Commit: Patch security vulnerability in authentication
