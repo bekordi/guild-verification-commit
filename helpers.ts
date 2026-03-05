9727e5c52df7ba61ff135b8acb450c47e6fc18f1
bekordi <150627764+bekordi@users.noreply.github.com>
2026-03-05 21:59:24 +0300

    Correct typo in configuration file

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T18:59:20.668Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T18:59:24.068Z
+Commit: Correct typo in configuration file
